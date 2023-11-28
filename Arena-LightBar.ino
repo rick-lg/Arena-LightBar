542281121a527993e0c15fbb2d510d66e015223d
rick-lg <rickpeaselg@gmail.com>
2023-11-28 18:59:42 -0500

    winner blink function

--- Arena-LightBar.ino
+++ Arena-LightBar.ino
@@ -30,7 +30,7 @@ void setup() {
 
 
     Serial.println("Here is the start");
-    delay( 3000 ); // power-up safety delay
+    delay( 2000 ); // power-up safety delay
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
     FastLED.setBrightness(  BRIGHTNESS );
     
@@ -43,12 +43,12 @@ static uint8_t startIndex = NUM_LEDS/2;
 
 void incrementStartIndex(){
     startIndex++;
-    startIndex = (startIndex>NUM_LEDS)?NUM_LEDS-1:startIndex;
+    startIndex = (startIndex>=NUM_LEDS)?NUM_LEDS-1:startIndex;
   
 }
 void decrementStartIndex(){  
     startIndex--;
-    startIndex = (startIndex<0)?0:startIndex;
+    startIndex = (startIndex<=0)?1:startIndex;
 }
 
 void ISR_blue_button_pressed(void) 
@@ -63,30 +63,51 @@ void ISR_red_button_pressed(void)
     delay(50);
 }
 
+bool flip = false;
+void victoryDance(CRGB Winner){
+   for(int i = 0; i < NUM_LEDS; i++) { 
+    leds[i] = CRGB::Black;
+  }   
+    FastLED.show();
+    FastLED.delay(200);   
+   for(int i = 0; i < NUM_LEDS; i++) { 
+    leds[i] = Winner;
+  }   
+    FastLED.show();
+    FastLED.delay(200);   
+}
+
 void loop()
 {
+    if(startIndex <= 1){
+      //Red wins
+     victoryDance(CRGB::Red);
+      
+    }else if(startIndex >= NUM_LEDS-1){
+      //Blue wins
+     victoryDance(CRGB::Blue);
+    }else{
+    for(int i = 0; i < startIndex; i++) { 
+       leds[i] = CRGB::Blue;
+    } 
     
-  for(int i = 0; i < startIndex; i++) { 
-     leds[i] = CRGB::Blue;
-  } 
-  
-  for(int i = 0; i < YELLOW_LED_WIDTH; i++){
-    leds[startIndex+i] = CRGB::Yellow;
-  }  
-  
-  for(int i = startIndex+YELLOW_LED_WIDTH; i < NUM_LEDS; i++) { 
-    leds[i] = CRGB::Red;
-  } 
-  FastLED.show(); 
-  
-  if(digitalRead(JUDGE0_BLUE_BUTTON) == LOW){
-    incrementStartIndex();
-  }
-  if(digitalRead(JUDGE0_RED_BUTTON) == LOW){
-    decrementStartIndex();
-  }
-
-  FastLED.delay(1000 / UPDATES_PER_SECOND);
+    for(int i = 0; i < YELLOW_LED_WIDTH; i++){
+      leds[startIndex+i] = CRGB::Yellow;
+    }  
     
+    for(int i = startIndex+YELLOW_LED_WIDTH; i < NUM_LEDS; i++) { 
+      leds[i] = CRGB::Red;
+    } 
+    FastLED.show(); 
+    
+    if(digitalRead(JUDGE0_BLUE_BUTTON) == LOW){
+      incrementStartIndex();
+    }
+    if(digitalRead(JUDGE0_RED_BUTTON) == LOW){
+      decrementStartIndex();
+    }
+    FastLED.delay(1000 / UPDATES_PER_SECOND);
+    
+    }  
 }
     
